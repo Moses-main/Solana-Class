b6c58184c8153b90da3fc615dd26c26b5c2b2564
Moses <Moseschizaram8@gmail.com>
2024-05-11 21:22:11 +0100

    Tried getting user input and then using it in a sentence

--- console_input/src/main.rs
+++ console_input/src/main.rs
@@ -1,8 +1,11 @@
-use std::io; // module to accept input from the user
+use std::io; // importing the std library to get the io module from it.
 
 fn main() {
-    let mut input = String::new();
+    println!("Enter your name: ");
+    let mut user_input = String::new();
 
-    io::stdin().read_line(&mut input).expect("Error reading input");
-    println!("{}", input)
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Error reading input");
+    println!("your name is {}", user_input)
 }
